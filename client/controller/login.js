2bb8710c7d937a7d599015415cdef4e1a6730527
Albin <albin.blent@gmail.com>
2014-10-11 18:09:37 +0200

    controllers

--- client/controller/login.js
+++ client/controller/login.js
@@ -1,9 +1,27 @@
 app.controller("login-ctrl", function($scope, $http, $location) 
 {
 
+$scope.validateUser = function (username,password){
+
+//check if user and pass is valid
+
+return true
+}
+
+$scope.loginButtonPressed = function (username,password){
+
+//call validate user
+
+//redirect to home page or give error message, user not found/pass is wrong
+
+}
+
+$scope.createUserButtonPressed = function (){
+
+//redirect to create user page
 
 
-
+}
 
 
 
