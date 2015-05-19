8a9be305c7c9dd52cb7713214b72158736ac150b
AlbinBlent <albin.blent@gmail.com>
2015-05-19 21:54:09 +0200

    la index på top invån

--- client/js/app.js
+++ client/js/app.js
@@ -6,32 +6,32 @@ app.config(function($routeProvider) {
 
 		// route for the default page
 		.when('/', {
-			templateUrl : 'view/login.html',
+			templateUrl : 'client/view/login.html',
 		})
 
 		// route for the create user page
 		.when('/create-user', {
-			templateUrl : 'view/create-user.html',
+			templateUrl : 'client/view/create-user.html',
 		})
 
 		// route for the group view page
 		.when('/group-view', {
-			templateUrl : 'view/group-view.html',
+			templateUrl : 'client/view/group-view.html',
 		})
 
 		// route for the create group page
 		.when('/create-group', {
-			templateUrl : 'view/create-group.html',
+			templateUrl : 'client/view/create-group.html',
 		})
 
 		// route for the login page
 		.when('/login', {
-			templateUrl : 'view/login.html',
+			templateUrl : 'client/view/login.html',
 		})
 
 		// route for the home page
 		.when('/home-page', {
-			templateUrl : 'view/home-page.html',
+			templateUrl : 'client/view/home-page.html',
 		});
 });
 
